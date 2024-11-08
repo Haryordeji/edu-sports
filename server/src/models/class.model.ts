e21c3535dd26740d80a5407fe5535edef32e33cb
Ayo Ogunsanya <aaolamiji@gmail.com>
2024-11-08 12:43:56 -0500

    model changes, need to generate new migration

--- server/src/models/class.model.ts
+++ server/src/models/class.model.ts
@@ -3,33 +3,36 @@ import { DataTypes, Model, Optional, Sequelize } from 'sequelize';
 
 interface ClassAttributes {
   class_id: UUID;
-  name: string;
-  skill_level: string;
-  start_time: Date;
-  end_time: Date;
+  title: string;
+  start: Date;
+  end: Date;
   location: string;
+  instructor: string;
+  level: number;
 }
 
 interface ClassCreationAttributes extends Optional<ClassAttributes, 'class_id'> {}
 
 export class Class extends Model<ClassAttributes, ClassCreationAttributes> implements ClassAttributes {
   public class_id!: UUID;
-  public name!: string;
-  public skill_level!: string;
-  public start_time!: Date;
-  public end_time!: Date;
+  public title!: string;
+  public start!: Date;
+  public end!: Date;
   public location!: string;
+  public instructor!: string;
+  public level!: number;
 }
 
 export const ClassModelInit = (sequelize: Sequelize) => {
   Class.init(
     {
       class_id: { type: DataTypes.UUID, primaryKey: true },
-      name: { type: DataTypes.STRING, allowNull: false },
-      skill_level: { type: DataTypes.STRING },
-      start_time: { type: DataTypes.DATE, allowNull: false },
-      end_time: { type: DataTypes.DATE, allowNull: false },
+      title: { type: DataTypes.STRING, allowNull: false },
+      start: { type: DataTypes.DATE, allowNull: false },
+      end: { type: DataTypes.DATE, allowNull: false },
       location: { type: DataTypes.STRING },
+      instructor: {type: DataTypes.STRING},
+      level: {type: DataTypes.INTEGER}
     },
     { sequelize, tableName: 'classes' }
   );
