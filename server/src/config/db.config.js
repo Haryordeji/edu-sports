37eab0659e790ab3d237832138e8b5ea3c492c64
Brandon <bambetsa@princeton.edu>
2024-11-15 15:42:01 -0500

    minor url render

--- server/src/config/db.config.js
+++ server/src/config/db.config.js
@@ -1,2 +1,21 @@
 require('ts-node/register');
 module.exports = require('./db.config.ts').default;
+
+module.exports = {
+    development: {
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      host: process.env.DB_HOST,
+    },
+    production: {
+      use_env_variable: 'DATABASE_URL',
+      dialect: 'postgres',
+      dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+            },
+        },
+    },
+  };
