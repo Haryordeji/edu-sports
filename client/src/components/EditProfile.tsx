116cc2d129e9c5277ab5c248c3888e40131dbc49
Jules Mpano <mpanojulesoreste@gmail.com>
2024-12-03 23:48:51 -0500

    {changes on the logo across the project}

--- client/src/components/EditProfile.tsx
+++ client/src/components/EditProfile.tsx
@@ -11,8 +11,8 @@ interface ProfileResponse {
 }
 const styles = {
   logo: {
-    width: '200px',
-    height: 'auto',
+    width: 'auto',
+    height: '80px',
   },
 };
 
@@ -127,7 +127,7 @@ const EditProfile: React.FC = () => {
     <div>
       <div className="registration-form">
         <div className="registration-header">
-        <img src="https://static.wixstatic.com/media/09e86e_318df3ef05b647329554c64770b3fd61~mv2.jpg/v1/fill/w_658,h_226,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Edu%20Sports%20Logo_04-01.jpg" alt="Company Logo" style={styles.logo} onClick={() => navigate(0)} />
+        <img src="/swing2tee_logo.png" alt="Swing 2 Tee Logo" style={styles.logo} onClick={() => navigate(0)} />
           <h1>Edu-Sports Academy</h1>
           </div>
           <button 
